054a7e2dbcdf1b74c7f578e2c488340c02fa22de
Namit2003 <namit.k.patel@gmail.com>
2025-10-18 16:51:19 -0400

    import instead of require

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const cors = require('cors')
-const router = require('./routes')
+import express from 'express'
+import cors from 'cors'
+import router from './routes.js'
 
 const app = express()
 app.use(express.json())
@@ -10,4 +10,4 @@ app.use(router)
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
-module.exports = app
+export default app
